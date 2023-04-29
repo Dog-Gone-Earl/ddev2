01e96dc66195ea147dba38ca965955a84afdf612
Terry Dennison Jr <107069502+Dog-Gone-Earl@users.noreply.github.com>
2023-04-29 08:46:38 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
+
 #bash script_name
+#eval "$(curl https://raw.githubusercontent.com/Dog-Gone-Earl/ddev2/main/setup.sh)"
 
 sudo apt update -y && sudo apt upgrade -y
 #install pyenv
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl python-all-dev git \
+unixodbc unixodbc-dev tdsodbc libunwind-dev liblz4-dev python-pip python3-pip
 
 curl -s https://pyenv.run | bash
 
-sudo sed -i '1 i\eval "$(pyenv init -)"' ~/.bashrc
+sudo sed -i '1 i\eval "$(pyenv init --path)"' ~/.bashrc
 sudo sed -i '1 i\command -v pyenv >/dev/null' ~/.bashrc
 sudo sed -i '1 i\export PATH="$PYENV_ROOT/bin:$PATH"' ~/.bashrc
 sudo sed -i '1 i\export PYENV_ROOT="$HOME/.pyenv"' ~/.bashrc
 sudo sed -i '1 i\eval "$(pyenv virtualenv-init -)"' ~/.bashrc
+
 source ~/.bashrc
 
 #install python 3.8.0
 pyenv update
 pyenv install 3.8.0
 
-#install pip
-sudo apt install python-pip -y
-sudo apt install python3-pip -y
-
 pyenv global 3.8.0
 
 python -m pip install --user pipx
 python3 -m pipx ensurepath
-python3 -m pip install --user -U pipx
 
-sudo apt-get -y install libunwind-dev
-sudo apt-get -y install liblz4-dev
 pip install --upgrade pip
 python3 -m pip install memray
 
@@ -43,3 +40,14 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+pip install datadog-checks-dev[cli]
+
+#open a new terminal with 'cmd+t'
+#vagrant ssh
+#pipx install ddev
+#ddev config set core /home/vagrant/integrations-core
+#ddev config set extras /home/vagrant/integrations-extras
+
+#to upgrade 
+#pip install --upgrade datadog-checks-dev[cli]
