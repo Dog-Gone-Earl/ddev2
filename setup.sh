2fc601f185e09c5c135a66a727ff8ca034ea38b1
Terry Dennison Jr <107069502+Dog-Gone-Earl@users.noreply.github.com>
2023-04-28 21:30:18 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,16 +8,16 @@ libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 
 #exec "$SHELL"
+sudo sed -i '1 i\eval "$(pyenv init -)"' ~/.bashrc
+#sudo sed -i '1 i\command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' ~/.bashrc
+sudo sed -i '1 i\command -v pyenv >/dev/null' ~/.bashrc
+sudo sed -i '1 i\export PATH="$PYENV_ROOT/bin:$PATH"' ~/.bashrc
+sudo sed -i '1 i\export PYENV_ROOT="$HOME/.pyenv"' ~/.bashrc
+sudo sed -i '1 i\eval "$(pyenv virtualenv-init -)"' ~/.bashrc
+source ~/.bashrc
 
 curl -s https://pyenv.run | bash
 
-sudo sed -i '1 i\export PYENV_ROOT="$HOME/.pyenv"' ~/.bashrc
-sudo sed -i '1 i\export PATH="$PYENV_ROOT/bin:$PATH"' ~/.bashrc
-sudo sed -i '1 i\eval "$(pyenv init -)"' ~/.bashrc
-sudo sed -i '1 i\eval "$(pyenv virtualenv-init -)"' ~/.bashrc
-sudo sed -i '1 i\command -v pyenv >/dev/null' ~/.bashrc
-
-source ~/.bashrc
 
 pyenv update
 
@@ -32,7 +32,7 @@ pyenv global 3.8.0
 python -m pip install --user pipx
 python3 -m pipx ensurepath
 python3 -m pip install --user -U pipx
-
+#source ~/.bashrc
 #source  .~/.bashrc
 
 sudo apt-get -y install libunwind-dev
